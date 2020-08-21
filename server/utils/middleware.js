345760027642afd4c1864f3d92a17a0478e161e9
Sean Ye <seanye24@gmail.com>
2020-08-21 16:35:31 -0700

    add error handling for jwt auth

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -59,13 +59,18 @@ const unknownEndpoint = (req, res) => {
 };
 
 const errorHandler = (error, req, res, next) => {
-  logger.error(error.message);
+  // logger.error(error.message);
 
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformed id' });
+    return res.status(400).json({ error: 'malformed id' });
   }
   if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
+  }
+  if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({
+      error: 'invalid token',
+    });
   }
   next(error);
 };
