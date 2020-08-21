8b2800d24c814181908f42596324c7e06c3e145e
Sean Ye <seanye24@gmail.com>
2020-08-21 16:35:08 -0700

    add jwt key to env var

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 
-const { PORT } = process.env;
-let { MONGODB_URI } = process.env;
+let { PORT, SECRET_KEY, MONGODB_URI } = process.env;
 
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
-module.exports = { PORT, MONGODB_URI };
+module.exports = { PORT, SECRET_KEY, MONGODB_URI };
