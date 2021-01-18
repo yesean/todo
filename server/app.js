218f1fd9c76025e558ca6a9ccba190c96c209522
Sean Ye <seanye24@gmail.com>
2021-01-18 15:09:30 -0800

    Fall back to react app

--- server/app.js
+++ server/app.js
@@ -31,14 +31,14 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.use('/api/users', usersRouter);
 app.use('/api/todos', todosRouter);
 app.use('/api/login', loginRouter);
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(middleware.errorHandler);
 
