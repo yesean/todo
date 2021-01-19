6de9139f569ee025b05d40fd15438d0ed0246f52
Sean Ye <seanye24@gmail.com>
2021-01-18 16:57:30 -0800

    Add react build

---
+++ server/build/precache-manifest.44b202e7d8a0eeb9ff10fc060d0f1cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b1b5161ce948e3ad75d6418adbfd2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4eb2978658971ca46db",
+    "url": "/static/css/main.5c77c46e.chunk.css"
+  },
+  {
+    "revision": "af323679f915444852a8",
+    "url": "/static/js/2.b75fcc85.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/static/js/2.b75fcc85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4eb2978658971ca46db",
+    "url": "/static/js/main.ec7e8422.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
